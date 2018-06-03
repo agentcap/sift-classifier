f7501e95fffe6e7397d41ab9a819aec0767e8c40
Pradeep Yarlagadda <pradeep.yarlagadda@students.iiit.ac.in>
2018-06-03 17:28:09 +0530

    Add train and testing along with CIFAR-10 dataset

---
+++ README.md
@@ -0,0 +1,5 @@
+# classification
+A basic classifier written in matlab which extracts the SIFT features and use svm to train and predict.
+
+## Requirement
+VLFEAT library
---
+++ classify.m
@@ -0,0 +1,15 @@
+batch1 = load('data_batch_1.mat');
+batch2 = load('data_batch_2.mat');
+batch3 = load('data_batch_3.mat');
+batch4 = load('data_batch_4.mat');
+batch5 = load('data_batch_5.mat');
+
+classes = [0,3];
+images = [batch1.data(logical(sum(batch1.labels == classes,2)),:);batch2.data(logical(sum(batch2.labels == classes,2)),:);batch3.data(logical(sum(batch3.labels == classes,2)),:);batch4.data(logical(sum(batch4.labels == classes,2)),:);batch5.data(logical(sum(batch5.labels == classes,2)),:);];
+lables = [batch1.labels(logical(sum(batch1.labels == classes,2)));batch2.labels(logical(sum(batch2.labels == classes,2)));batch3.labels(logical(sum(batch3.labels == classes,2)));batch4.labels(logical(sum(batch4.labels == classes,2)));batch5.labels(logical(sum(batch5.labels == classes,2)));];
+[C,no_clusters,md2] = train(images,lables);
+
+t_batch = load('test_batch.mat');
+images = t_batch.data(logical(sum(t_batch.labels == classes,2)),:);
+lables = t_batch.labels(logical(sum(t_batch.labels == classes,2)));
+acc = test(images,lables,C,no_clusters,md2);
